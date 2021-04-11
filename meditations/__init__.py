f517293b021e36d48130d957993ee1141b09bca9
Rikki Hodder <rikkih@protonmail.com>
2021-04-11 23:46:46 +0100

    extensions handler

--- meditations/__init__.py
+++ meditations/__init__.py
@@ -3,12 +3,16 @@ import os
 
 from flask import Flask
 
+from .extensions import db
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev"
     )
+
+    db.init_app(app)
 
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
