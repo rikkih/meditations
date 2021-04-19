12d45c20ef2152f072bfd44e5c8a466dea98ceec
Rikki Hodder <rikkih@protonmail.com>
2021-04-19 23:20:47 +0100

    sqlalchemy track modifications deprecated so silened

--- meditations/config.py
+++ meditations/config.py
@@ -5,3 +5,4 @@ from uuid import uuid4
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY") or str(uuid4())
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
