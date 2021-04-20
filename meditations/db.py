ba15337fa9f339d78dbc223e8793d576218aa1ff
Rikki Hodder <rikkih@protonmail.com>
2021-04-20 22:56:10 +0100

    starting with a simpler structure to get things up and running with less concern

--- meditations/db.py
+++ meditations/db.py
@@ -1,13 +1,3 @@
-from flask import current_app, g
-
-from .extensions import db
-
-
-def get_db():
-    """Helper to get a connection from the database pool.
-
-    TODO: Use a flask.before_application_context function and handle the
-    database connection here. Return the connection to the pool at the end of
-    the request.
-    """
-    pass
+"""Placeholder for when the codebase becomes more wieldy and needs a
+structured layout and clear, separated logic. 
+"""
--- meditations/models.py
+++ meditations/models.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
-from db import db
+db = SQLAlchemy()
 
 
 class Post(db.model):
