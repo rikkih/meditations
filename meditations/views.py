9fd4e52db4b4646f341c0b9df4d75d59eb7531eb
Rikki Hodder <rikkih@protonmail.com>
2021-04-25 22:04:58 +0100

    first C endpoint

--- meditations/views.py
+++ meditations/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template
+
+from meditations.models import Post
 
 blueprint = Blueprint(
     "meditations",
@@ -12,3 +14,17 @@ blueprint = Blueprint(
 @blueprint.route("/home")
 def landing_page():
     return render_template("index.html")
+
+
+@blueprint.route("/posts/new", methods=["GET", "POST"])
+def create_post():
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["post"]
+        author = request.form["author"]
+        post = Post(title=title, content=content, author=author)
+        db.add(post)
+        db.commit()
+        return redirect("/posts")
+    else:
+        return render_template("new_post.html")
