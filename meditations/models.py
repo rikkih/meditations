8883984a62bf3775180aeca65590c3482fda3201
Rikki Hodder <rikkih@protonmail.com>
2021-05-03 18:15:46 +0100

    models overhaul

--- meditations/models.py
+++ meditations/models.py
@@ -2,23 +2,10 @@ from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.security import check_password_hash, generate_password_hash
 
 db = SQLAlchemy()
 migrate = Migrate()
-
-
-class Post(db.Model):
-    """Blog Post Model for a users posts."""
-    __tablename__ = "post"
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), nullable=False, unique=True)
-    content = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(32), nullable=False, default="MissingNo")
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f"<Post {self.title}>"
 
 
 class User(db.Model):
@@ -31,6 +18,27 @@ class User(db.Model):
     name = db.Column(db.String(32), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     created_on = db.Column(db.DateTime, default=datetime.utcnow)
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
 
     def __repr__(self):
         return f"<User {self.name}>"
+
+    def set_password(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password) -> bool:
+        return check_password_hash(self.password_hash, password)
+
+
+class Post(db.Model):
+    """Blog Post Model for a users posts."""
+    __tablename__ = "post"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False, unique=True)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        return f"<Post {self.title}>"
